80f1529ed87eb0a1f080cea7450fab7531daaa74
kdh23232 <kdh23232@naver.com>
2023-09-22 15:53:13 +0900

    20230922

---
+++ 11_ajax/src/main/java/service/MemberService.java
@@ -0,0 +1,16 @@
+package service;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface MemberService {
+
+  public void getMemberList(HttpServletResponse response) throws IOException;  // printwriter가 예외처리가 필요한 코드임. 그래서 던져부리기
+  public void memberAdd(HttpServletRequest request, HttpServletResponse response) throws IOException;
+  public void memberEmailCheck(HttpServletRequest request, HttpServletResponse response) throws IOException;
+  public void memberDetail(HttpServletRequest request, HttpServletResponse response) throws IOException;
+  public void memberModify(HttpServletRequest request, HttpServletResponse response) throws IOException;
+  public void memberDelete(HttpServletRequest request, HttpServletResponse response) throws IOException;
+}
